9c3a3cee827f543787617e23b2c8b2367d6295ed
David Mbaimbai <dmbaimbai@csod.com>
2024-04-17 12:23:10 +0200

    Assessment

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { RegisterComponent } from './auth/register/register.component';
 import { TransactionHistoryComponent } from './components/transaction-history/transaction-history.component';
 import { CreditTransactionComponent } from './components/credit-transaction/credit-transaction.component';
 import { DebitTransactionComponent } from './components/debit-transaction/debit-transaction.component';
-import { TransferTransactionComponent } from './components/transfer-transaction/transfer-transaction.component';
 import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
@@ -14,7 +13,6 @@ export const routes: Routes = [
   { path: 'trans-history', component: TransactionHistoryComponent, canActivate: [AuthGuard]},
   { path: 'trans-credit', component: CreditTransactionComponent, canActivate: [AuthGuard]},
   { path: 'trans-debit', component: DebitTransactionComponent, canActivate: [AuthGuard]},
-  { path: 'transfer', component: TransferTransactionComponent, canActivate: [AuthGuard]},
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
