0899ed849991aef6add4b9778622fa65ec54ab4e
David Mbaimbai <dmbaimbai@csod.com>
2024-04-17 13:54:25 +0200

    Assessment

--- wallet/src/main/java/com/walletapp/controller/TransactionHistoryController.java
+++ wallet/src/main/java/com/walletapp/controller/TransactionHistoryController.java
@@ -14,10 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,6 +24,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 @RequestMapping("/api/transaction")
 @AllArgsConstructor
 @Tag(name = "Transaction history  API")
